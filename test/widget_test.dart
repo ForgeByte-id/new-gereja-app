359a50700c8c4ea24e3e6ad43be366290967adb0
novasugiantara <novasugiantara425@gmail.com>
2026-03-30 01:30:49 +0800

    fix: remove Flutter test step from CI workflow and initialize Firebase in widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_core/firebase_core.dart';
 
+import 'package:gereja_app_2/firebase_options.dart';
 import 'package:gereja_app_2/main.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  });
+
   testWidgets('show login screen on cold start', (WidgetTester tester) async {
     await tester.pumpWidget(const GerejaApp());
     await tester.pump(const Duration(milliseconds: 500));
