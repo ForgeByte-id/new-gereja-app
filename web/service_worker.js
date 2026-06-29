24b6e9bab7724d6f5c2bf0deb10d168d368a2cb7
novasugiantara <novasugiantara425@gmail.com>
2026-06-29 19:20:13 +0800

    add service_worker.js

---
+++ web/service_worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "gpi-yehuda-v1";
+
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/favicon.png",
+  "/icons/Icon-192.png",
+  "/icons/Icon-512.png",
+  "/icons/Icon-maskable-192.png",
+  "/icons/Icon-maskable-512.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
+
+self.addEventListener("beforeinstallprompt", () => {});
